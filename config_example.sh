ca491d486176db2d0099494fd115c1c85b86607b
Dan Winship <danw@redhat.com>
2019-11-14 14:25:21 -0500

    Alphabetize config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Generic configuration
+
 # A namespace where builds will be executed
 IPV6_NAMESPACE=ipv6
 
@@ -13,29 +15,7 @@ IPV6_KUBECONFIG=ipv6-kubeconfig
 # images referenced by the payload - are hosted
 IPV6_PULLSECRET=ipv6-pullsecret
 
-# machine-config-operator build config
-MCO_STREAM=machine-config-operator
-MCO_GIT_URI=https://github.com/russellb/machine-config-operator.git
-MCO_GIT_REF=ipv6
-MCO_DOCKERFILE=Dockerfile
-
-# cluster-network-operator build config
-CNO_STREAM=cluster-network-operator
-CNO_GIT_URI=https://github.com/markmc/cluster-network-operator.git
-CNO_GIT_REF=ipv6-hack
-CNO_DOCKERFILE=Dockerfile
-
-# ovn-kubernetes build config
-OVNKUBE_STREAM=ovn-kubernetes
-OVNKUBE_GIT_URI=https://github.com/markmc/ovn-kubernetes.git
-OVNKUBE_GIT_REF=ipv6-hack
-OVNKUBE_DOCKERFILE=Dockerfile
-
-# hyperkube build config
-HYPERKUBE_STREAM=hyperkube
-HYPERKUBE_GIT_URI=https://github.com/danwinship/origin.git
-HYPERKUBE_GIT_REF=ipv6
-HYPERKUBE_DOCKERFILE=images/hyperkube/Dockerfile.rhel
+## Specific modules
 
 # cluster-kube-apiserver-operator build config
 CKAO_STREAM=cluster-kube-apiserver-operator
@@ -43,10 +23,35 @@ CKAO_GIT_URI=https://github.com/danwinship/cluster-kube-apiserver-operator.git
 CKAO_GIT_REF=ipv6-bind
 CKAO_DOCKERFILE=Dockerfile
 
+# cluster-network-operator build config
+CNO_STREAM=cluster-network-operator
+CNO_GIT_URI=https://github.com/markmc/cluster-network-operator.git
+CNO_GIT_REF=ipv6-hack
+CNO_DOCKERFILE=Dockerfile
+
+# cluster-version-operator build config
 CVO_STREAM=cluster-version-operator
 CVO_GIT_URI=https://github.com/russellb/cluster-version-operator.git
 CVO_GIT_REF=ipv6-hack
 CVO_DOCKERFILE=Dockerfile
 
+# hyperkube build config
+HYPERKUBE_STREAM=hyperkube
+HYPERKUBE_GIT_URI=https://github.com/danwinship/origin.git
+HYPERKUBE_GIT_REF=ipv6
+HYPERKUBE_DOCKERFILE=images/hyperkube/Dockerfile.rhel
+
+# machine-config-operator build config
+MCO_STREAM=machine-config-operator
+MCO_GIT_URI=https://github.com/russellb/machine-config-operator.git
+MCO_GIT_REF=ipv6
+MCO_DOCKERFILE=Dockerfile
+
 # machine-os-content build config
 MOC_STREAM=machine-os-content
+
+# ovn-kubernetes build config
+OVNKUBE_STREAM=ovn-kubernetes
+OVNKUBE_GIT_URI=https://github.com/markmc/ovn-kubernetes.git
+OVNKUBE_GIT_REF=ipv6-hack
+OVNKUBE_DOCKERFILE=Dockerfile
