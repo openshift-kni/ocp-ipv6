59352e0b508845173175564ca842873112d556cd
Mark McLoughlin <markmc@redhat.com>
2019-11-27 13:46:34 +0000

    machine-os-content build should use our builds of hyperkube

--- build_machine_os_content.sh
+++ build_machine_os_content.sh
@@ -9,7 +9,7 @@ BASE_RELEASE_PULLSPEC="$1"; shift || true
 HYPERKUBE_PULLSPEC="$1"; shift || true
 if [ -z "${MOC_VERSION}" -o -z "${BASE_RELEASE_PULLSPEC}" -o -z "${HYPERKUBE_PULLSPEC}" ]; then
     echo "usage: $0 <output version> <base release pullspec> <hyperkube pullspec>" >&2
-    echo "example: $0 4.3.0.ipv6-2019-11-01-0001 registry.svc.ci.openshift.org/ocp/release:4.3.0-0.ci-2019-11-01-112322 quay.io/danwinship/hyperkube:ipv6" >&2
+    echo "example: $0 4.3.0.ipv6-2019-11-01-0001 registry.svc.ci.openshift.org/ocp/release:... registry.svc.ci.openshift.org/ipv6/hyperkube@sha256:..." >&2
     exit 1
 fi
 
